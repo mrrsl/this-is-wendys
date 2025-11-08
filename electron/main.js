3a4d0beacd3cc92200a0828461c3c86243cd1811
Morris L <morrislli@outlook>
2025-11-08 10:59:02 -0800

    added preview page

--- electron/main.js
+++ electron/main.js
@@ -1,26 +1,32 @@
 const { app, BrowserWindow } = require("electron/main");
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+	
+	let targetFile = (process.env.PREVIEW) ? "./preview/preview.html" : "index.html";
+	console.log("PREVIEW=", process.env.PREVIEW);
+ 	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+  	});
 
-  win.loadFile("index.html");
+  	win.loadFile(targetFile);
 };
 
 app.whenReady().then(() => {
-  createWindow();
+  	createWindow();
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+  	app.on("activate", () => {
+		const windows = BrowserWindow.getAllWindows();
+		if (windows.length === 0) {
+	  		createWindow();
+		} else {
+	  
+		}
+  	});
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  	if (process.platform !== "darwin") {
+		app.quit();
+  	}
 });
