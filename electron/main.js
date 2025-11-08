57c44cc5316a6904e1e89bed0af51f245bbec39f
SadRectangle <graceyang803@gmail.com>
2025-11-08 12:39:32 -0800

    font

--- electron/main.js
+++ electron/main.js
@@ -1,32 +1,33 @@
+import "style.css";
 const { app, BrowserWindow } = require("electron/main");
 
 const createWindow = () => {
-	
-	let targetFile = (process.env.PREVIEW) ? "./preview/preview.html" : "index.html";
-	console.log("PREVIEW=", process.env.PREVIEW);
- 	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-  	});
+  let targetFile = process.env.PREVIEW
+    ? "./preview/preview.html"
+    : "index.html";
+  console.log("PREVIEW=", process.env.PREVIEW);
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-  	win.loadFile(targetFile);
+  win.loadFile(targetFile);
 };
 
 app.whenReady().then(() => {
-  	createWindow();
+  createWindow();
 
-  	app.on("activate", () => {
-		const windows = BrowserWindow.getAllWindows();
-		if (windows.length === 0) {
-	  		createWindow();
-		} else {
-	  
-		}
-  	});
+  app.on("activate", () => {
+    const windows = BrowserWindow.getAllWindows();
+    if (windows.length === 0) {
+      createWindow();
+    } else {
+    }
+  });
 });
 
 app.on("window-all-closed", () => {
-  	if (process.platform !== "darwin") {
-		app.quit();
-  	}
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
